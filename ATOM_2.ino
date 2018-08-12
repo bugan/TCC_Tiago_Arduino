11e768d366a364a866b94403f486078067727a3a
Ricardo <dones.bugan@gmail.com>
2018-08-11 23:46:58 -0300

    Continuando refatoração

--- ATOM_2.ino
+++ ATOM_2.ino
@@ -1,7 +1,12 @@
 const bool DEVE_TESTAR = false;
+const int DISTANCIA_SEGURA = 10;
+const int PINO_MOTOR_DIREITA = 5; //pwm direito - cinza
+const int PINO_MOTOR_ESQUERDA = 6; //pwm esquerdo - laranja
+const int VOLTAGEM_MAXIMA_EM_SUPERFICIE_CLARA = 380;
+const int VOLTAGEM_MINIMA_EM_CIMA_DA_LINHA_AZUL = 400; 
+const int VOLTAGEM_MINIMA_EM_CIMA_DA_LINHA_PRETA = 700;
+const float VELOCIDADE_DO_SOM = 29.4117647;
 
-const int velr = 5; //pwm direito - cinza
-const int vell = 6; //pwm esquerdo - laranja
 int para = 0;
 
 
@@ -9,19 +14,19 @@ int para = 0;
 int gatilho = 2; // pino TRIG do sensor ultrassônico
 int echo = 3; // pino ECHO do sensor ultrassônico
 float tempo; // para armazenar o tempo de ida e volta do sinal em microsegundos
-float distancia_cm; // para armazenar a distância em centímetros
+float distanciaEmCentimetros; // para armazenar a distância em centímetros
 
 
 //variáveis para o sensor infravermelho
-int sensorValue0 = 0;
-int sensorValue1 = 0;
-int sensorValue2 = 0;
-int sensorValue3 = 0;
-int sensorValue4 = 0;
+int sensorVelocidade = 0;
+int sensorExternoEsquerda = 0;
+int sensorInternoDireita = 0;
+int sensorInternoEsquerda = 0;
+int sensorExternoDireta = 0;
 
-#define linha 380 //cor máxima para a superfície
-#define LinhaBaixa 400 //cor mínima para a faixa azul
-#define LinhaAlta 700 //cor mínima para a faixa preta
+int valorMaximoSensorVelocidade;
+
+
 
 void setup() {
 
@@ -32,8 +37,8 @@ void setup() {
     pinMode(10, OUTPUT); // Define pino D10 como saída - entrada I2 - motor direito - azul
     pinMode(12, OUTPUT); // Define pino D12 como saída - entrada I3 - motor esquerdo - verde
     pinMode(11, OUTPUT); // Define pino D11 como saída - entrada I4 - motor esquerdo - amarelo
-    pinMode(velr, OUTPUT); // Define pino D5 como saída
-    pinMode(vell, OUTPUT); // define pine D6 como saída
+    pinMode(PINO_MOTOR_DIREITA, OUTPUT); // Define pino D5 como saída
+    pinMode(PINO_MOTOR_ESQUERDA, OUTPUT); // define pine D6 como saída
 
 
     // Configuração do Arduino para o sensor ultrassônico HC-SR04
@@ -43,141 +48,163 @@ void setup() {
     pinMode(echo, INPUT); // Configura pino ECHO como entrada
 }
 
-void TestarValoresDeEntradaDosSensores(){
+void TestarValoresDeEntradaDosSensores() {
     // Apresentar resultados no monitor serial
-    Serial.print("sensor0: ");
-    Serial.println(sensorValue0);
-    Serial.print("sensor1: ");
-    Serial.println(sensorValue1);
-    Serial.print("sensor2: ");
-    Serial.println(sensorValue2);
-    Serial.print("sensor3: ");
-    Serial.println(sensorValue3);
-    Serial.print("sensor4: ");
-    Serial.println(sensorValue4);
+    Serial.print("sensor velocidade: ");
+    Serial.println(sensorVelocidade);
+
+    Serial.print("sensor Externo Direita: ");
+    Serial.println(sensorExternoDireta);
+
+    Serial.print("sensor Externo esquerda: ");
+    Serial.println(sensorExternoEsquerda);
+
+    Serial.print("sensor Interno Direita: ");
+    Serial.println(sensorInternoDireita);
+
+    Serial.print("sensor Interno Esquerda: ");
+    Serial.println(sensorInternoEsquerda);
+}
+
+void TestarUltrassom(){
+    // Apresentar resultados no monitor serial
+    Serial.print("Distancia: ");
+    Serial.print(distanciaEmCentimetros);
+    Serial.println(" cm");
 }
 
 void loop() {
 
-    sensorValue0 = analogRead(A0);
-    sensorValue1 = analogRead(A1);
-    sensorValue2 = analogRead(A2);
-    sensorValue3 = analogRead(A3);
-    sensorValue4 = analogRead(A4);
+    LerEntradaDosSensores();
+    DispararUltrasom();
+    distanciaEmCentimetros = CalcularDistancia();
+    
 
+    if (distanciaEmCentimetros > DISTANCIA_SEGURA) {
+        AjustarVelocidade();
+        VerificarSensores(sensorInternoEsquerda, sensorInternoDireita, VirarParaDireita, VirarParaEsquerda);
+        VerificarSensores(sensorExternoEsquerda, sensorExternoDireta, Virar90GrausDireita, Virar90GrausEsquerda);
+    }
+    else if (distanciaEmCentimetros < DISTANCIA_SEGURA) {
+        Parar();
+    }
+    
+    if(DEVE_TESTAR){
+        TestarValoresDeEntradaDosSensores();
+        TestarUltrassom();
+    }
+    
+}
 
+void LerEntradaDosSensores(){
+    sensorVelocidade = analogRead(A0);
+    sensorExternoEsquerda = analogRead(A1);
+    sensorInternoDireita = analogRead(A2);
+    sensorInternoEsquerda = analogRead(A3);
+    sensorExternoDireta = analogRead(A4);
+}
 
+void MudarVelocidade(int velocidade){
+    analogWrite(PINO_MOTOR_DIREITA, velocidade); //PWM configurado para 2V (40%) para o motor A
+    analogWrite(PINO_MOTOR_ESQUERDA, velocidade); //PWM configurado para 2V (40%) para o motor B
+}
 
-    // Disparo do pulso ultrassônico
+float CalcularDistancia(){
+    tempo = pulseIn(echo, HIGH); // Mede o tempo de ida e volta do pulso ultrassônico
+    return tempo / VELOCIDADE_DO_SOM / 2; // Calcula as distâncias em centímetros    
+}
+
+void DispararUltrasom(){
     digitalWrite(gatilho, HIGH);
     delayMicroseconds(10);
     digitalWrite(gatilho, LOW);
+}
 
-    tempo = pulseIn(echo, HIGH); // Mede o tempo de ida e volta do pulso ultrassônico
-
-    distancia_cm = tempo / 29.4117647 / 2; // Calcula as distâncias em centímetros
-
-    /*// Apresentar resultados no monitor serial
-    Serial.print("Distancia: ");
-    Serial.print(distancia_cm);
-    Serial.println(" cm");*/
-
-    // Verificação e comando do protótipo de AGV
-    if (distancia_cm > 10) {
-
-        if (sensorValue0 < LinhaBaixa) {
-            //condição de arranque
-
-            analogWrite(velr, 102); //PWM configurado para 2V (40%) para o motor A
-            analogWrite(vell, 102); //PWM configurado para 2V (40%) para o motor B
-
-
-        }
-
-        if (sensorValue0 >= LinhaBaixa && sensorValue0 < LinhaAlta) {
-
-            analogWrite(velr, 102); //PWM configurado para 2V (40%) para o motor A
-            analogWrite(vell, 102); //PWM configurado para 2V (40%) para o motor B
-        }
-        else if (sensorValue0 >= LinhaAlta) {
-
-            analogWrite(velr, 120); //PWM configurado para 3V (60%) para o motor A
-            analogWrite(vell, 120); //PWM configurado para 3V (60%) para o motor B
-        }
-        VerificarSensoresInternos();
-        VerificarSensoresExternos();
-
+void AjustarVelocidade(){
+    if (sensorVelocidade < VOLTAGEM_MINIMA_EM_CIMA_DA_LINHA_PRETA) {
+        MudarVelocidade(102);
     }
-    else if (distancia_cm < 10) {
-        //para
-        analogWrite(velr, 0); //PWM configurado para 0V (0%) para o motor A
-        analogWrite(vell, 0); //PWM configurado para 0V (0%) para o motor B
+    else{
+        MudarVelocidade(120);
     }
 }
 
-void VerificarSensoresInternos(){
-    if (!EstaForaDaLinha(sensorValue2) && !EstaForaDaLinha(sensorValue3)) {
-        //anda para frente
-        digitalWrite(9, HIGH);
-        digitalWrite(10, LOW);
-        digitalWrite(11, HIGH);
-        digitalWrite(12, LOW);
-    }
-    else if (EstaForaDaLinha(sensorValue2) && !EstaForaDaLinha(sensorValue3)) {
-        //vira para esquerda
-        digitalWrite(9, HIGH);
-        digitalWrite(10, LOW);
-        digitalWrite(11, LOW);
-        digitalWrite(12, LOW);
-    }
-    else if (!EstaForaDaLinha(sensorValue2) && EstaForaDaLinha(sensorValue3)) {
-        //vira para direita
-        digitalWrite(9, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(11, HIGH);
-        digitalWrite(12, LOW);
-    }
-}
-
-void VerificarSensoresExternos(){
-
-    if (EstaForaDaLinha(sensorValue1) && !EstaForaDaLinha(sensorValue4)) {
-        //vira para esquerda (90º)
-        analogWrite(velr, 102); //PWM configurado para 2V (40%) para o motor A
-        analogWrite(vell, 102); //PWM configurado para 2V (40%) para o motor B
-        digitalWrite(9, HIGH);
-        digitalWrite(10, LOW);
-        digitalWrite(11, LOW);
-        digitalWrite(12, HIGH);
-    }
-    else if (!EstaForaDaLinha(sensorValue1)&& EstaForaDaLinha(sensorValue4)) {
-        //vira para direita (90º)
-        analogWrite(velr, 102); //PWM configurado para 2V (40%) para o motor A
-        analogWrite(vell, 102); //PWM configurado para 2V (40%) para o motor B
-        digitalWrite(9, LOW);
-        digitalWrite(10, HIGH);
-        digitalWrite(11, HIGH);
-        digitalWrite(12, LOW);
-    }
-    else if (EstaForaDaLinha(sensorValue1) && EstaForaDaLinha(sensorValue4)) {
-        //para
-        analogWrite(velr, 0); //PWM configurado para 0V (0%) para o motor A
-        analogWrite(vell, 0); //PWM configurado para 0V (0%) para o motor B
-        if (para == 0){
-            delay(1000);
-            //frente
-            analogWrite(velr, 120); //PWM configurado para 3V (60%) para o motor A
-            analogWrite(vell, 120); //PWM configurado para 3V (60%) para o motor B
-            digitalWrite(9, HIGH);
-            digitalWrite(10, LOW);
-            digitalWrite(11, HIGH);
-            digitalWrite(12, LOW);
-            delay(500);
-            para = 0;
+void VerificarSensores(int sensorEsquerda, int sensorDireita, void (*VirarDireta)(), void (*VirarEsquerda)())
+{
+    if (EstaDentroDaLinha(sensorEsquerda) && EstaDentroDaLinha(sensorDireita)) {
+        Parar();
+    } else {
+        if (EstaDentroDaLinha(sensorEsquerda)) {
+            VirarDireta();
+        } else if (EstaDentroDaLinha(sensorDireita)) {
+            VirarEsquerda();
+        }
+        else {
+            AndarParaFrente();
         }
     }
 }
 
-bool EstaForaDaLinha(float valorDoSensor){
-    return valorDoSensor > linha;
+
+void AguardarNaEstacao() {
+    if (para == 0) {
+        delay(1000);
+        //frente
+        analogWrite(PINO_MOTOR_DIREITA, 120); //PWM configurado para 3V (60%) para o motor A
+        analogWrite(PINO_MOTOR_ESQUERDA, 120); //PWM configurado para 3V (60%) para o motor B
+        digitalWrite(9, HIGH);
+        digitalWrite(10, LOW);
+        digitalWrite(11, HIGH);
+        digitalWrite(12, LOW);
+        delay(500);
+        para = 0;
+    }
+}
+
+void Parar() {
+    analogWrite(PINO_MOTOR_DIREITA, 0); //PWM configurado para 0V (0%) para o motor A
+    analogWrite(PINO_MOTOR_ESQUERDA, 0); //PWM configurado para 0V (0%) para o motor B
+}
+
+void VirarParaDireita() {
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, LOW);
+}
+
+void VirarParaEsquerda() {
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+}
+
+void Virar90GrausEsquerda() {
+    analogWrite(PINO_MOTOR_DIREITA, 102); //PWM configurado para 2V (40%) para o motor A
+    analogWrite(PINO_MOTOR_ESQUERDA, 102); //PWM configurado para 2V (40%) para o motor B
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(12, HIGH);
+}
+
+void Virar90GrausDireita() {
+    analogWrite(PINO_MOTOR_DIREITA, 102); //PWM configurado para 2V (40%) para o motor A
+    analogWrite(PINO_MOTOR_ESQUERDA, 102); //PWM configurado para 2V (40%) para o motor B
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, LOW);
+}
+
+void AndarParaFrente() {
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, LOW);
+}
+
+bool EstaDentroDaLinha(float valorDoSensor) {
+    return valorDoSensor >= VOLTAGEM_MINIMA_EM_CIMA_DA_LINHA_AZUL;
 }
